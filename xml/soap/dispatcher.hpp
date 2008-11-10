4292702833150b16b21b2f456cda6b34460f1dda
maarten <maarten@31ceaa29-a5dd-4727-bcb8-6a3098495666>
2008-11-10 20:57:48 +0000

    negen argumenten
    git-svn-id: svn+ssh://mail.hekkelman.net/home/maarten/svnroot/libzeep/trunk@4 31ceaa29-a5dd-4727-bcb8-6a3098495666

--- xml/soap/dispatcher.hpp
+++ xml/soap/dispatcher.hpp
@@ -11,17 +11,13 @@ namespace xml
 namespace soap
 {
 
-template<class C>
-struct call_helper
+template<class C, typename F>
+struct call_traits_base;
+
+template<class C, typename T1, typename T2, typename R>
+struct call_traits_base<C, void (C::*)(const T1&, const T2&, R&)>
 {
-	template<typename F>
-	struct traits;
-	
-	template<typename T1, typename T2, typename R>
-	struct traits<void (C::*)(const T1&, const T2&, R&)>
-	{
-		typedef typename call<C,void(const T1&, const T2&, R&)> call_type;
-	}
+	typedef call<C,void(const T1&, const T2&, R&)> type;
 };
 
 
@@ -51,22 +47,6 @@ class dispatcher : public boost::noncopyable
 							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
 					}
 
-//	template<typename F>
-//	void			register_soap_call(
-//						const char*		action,
-//						typename call<Derived,F>::call_func_type				call_,
-//						const char*		arg1,
-//						const char*		arg2)
-//					{
-//						enum { required_count = call<Derived,F>::name_count };
-//						BOOST_STATIC_ASSERT(required_count == 2);
-//						
-//						const char* names[2] = { arg1, arg2 };
-//						
-//						actions.push_back(
-//							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
-//					}
-
 	template<typename F>
 	void			register_soap_call(
 						const char*		action,
@@ -80,7 +60,147 @@ class dispatcher : public boost::noncopyable
 						const char* names[2] = { arg1, arg2 };
 						
 						actions.push_back(
-							new call<Derived,>(action, static_cast<Derived*>(this), call_, names));
+							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
+					}
+
+	template<typename F>
+	void			register_soap_call(
+						const char*		action,
+						F				call_,
+						const char*		arg1,
+						const char*		arg2,
+						const char*		arg3)
+					{
+						enum { required_count = call<Derived,F>::name_count };
+						BOOST_STATIC_ASSERT(required_count == 3);
+						
+						const char* names[3] = { arg1, arg2, arg3 };
+						
+						actions.push_back(
+							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
+					}
+
+	template<typename F>
+	void			register_soap_call(
+						const char*		action,
+						F				call_,
+						const char*		arg1,
+						const char*		arg2,
+						const char*		arg3,
+						const char*		arg4)
+					{
+						enum { required_count = call<Derived,F>::name_count };
+						BOOST_STATIC_ASSERT(required_count == 4);
+						
+						const char* names[4] = { arg1, arg2, arg3, arg4 };
+						
+						actions.push_back(
+							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
+					}
+
+	template<typename F>
+	void			register_soap_call(
+						const char*		action,
+						F				call_,
+						const char*		arg1,
+						const char*		arg2,
+						const char*		arg3,
+						const char*		arg4,
+						const char*		arg5)
+					{
+						enum { required_count = call<Derived,F>::name_count };
+						BOOST_STATIC_ASSERT(required_count == 5);
+						
+						const char* names[5] = { arg1, arg2, arg3, arg4, arg5 };
+						
+						actions.push_back(
+							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
+					}
+
+	template<typename F>
+	void			register_soap_call(
+						const char*		action,
+						F				call_,
+						const char*		arg1,
+						const char*		arg2,
+						const char*		arg3,
+						const char*		arg4,
+						const char*		arg5,
+						const char*		arg6)
+					{
+						enum { required_count = call<Derived,F>::name_count };
+						BOOST_STATIC_ASSERT(required_count == 6);
+						
+						const char* names[6] = { arg1, arg2, arg3, arg4, arg5, arg6 };
+						
+						actions.push_back(
+							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
+					}
+
+	template<typename F>
+	void			register_soap_call(
+						const char*		action,
+						F				call_,
+						const char*		arg1,
+						const char*		arg2,
+						const char*		arg3,
+						const char*		arg4,
+						const char*		arg5,
+						const char*		arg6,
+						const char*		arg7)
+					{
+						enum { required_count = call<Derived,F>::name_count };
+						BOOST_STATIC_ASSERT(required_count == 7);
+						
+						const char* names[7] = { arg1, arg2, arg3, arg4, arg5, arg6, arg7 };
+						
+						actions.push_back(
+							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
+					}
+
+	template<typename F>
+	void			register_soap_call(
+						const char*		action,
+						F				call_,
+						const char*		arg1,
+						const char*		arg2,
+						const char*		arg3,
+						const char*		arg4,
+						const char*		arg5,
+						const char*		arg6,
+						const char*		arg7,
+						const char*		arg8)
+					{
+						enum { required_count = call<Derived,F>::name_count };
+						BOOST_STATIC_ASSERT(required_count == 8);
+						
+						const char* names[8] = { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 };
+						
+						actions.push_back(
+							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
+					}
+
+	template<typename F>
+	void			register_soap_call(
+						const char*		action,
+						F				call_,
+						const char*		arg1,
+						const char*		arg2,
+						const char*		arg3,
+						const char*		arg4,
+						const char*		arg5,
+						const char*		arg6,
+						const char*		arg7,
+						const char*		arg8,
+						const char*		arg9)
+					{
+						enum { required_count = call<Derived,F>::name_count };
+						BOOST_STATIC_ASSERT(required_count == 9);
+						
+						const char* names[9] = { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 };
+						
+						actions.push_back(
+							new call<Derived,F>(action, static_cast<Derived*>(this), call_, names));
 					}
 
   private:
