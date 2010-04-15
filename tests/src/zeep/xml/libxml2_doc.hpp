45b85a1495cf03b354176ef297b656dca9bacf32
maarten <maarten@31ceaa29-a5dd-4727-bcb8-6a3098495666>
2010-04-15 13:59:58 +0000

    optimisations (but now cp() leaks allowed-ptrs)
    git-svn-id: svn+ssh://mail.hekkelman.net/home/maarten/svnroot/libzeep/trunk@151 31ceaa29-a5dd-4727-bcb8-6a3098495666

--- tests/src/zeep/xml/libxml2_doc.hpp
+++ tests/src/zeep/xml/libxml2_doc.hpp
@@ -13,23 +13,65 @@ namespace zeep { namespace xml {
 class libxml2_doc : public boost::noncopyable
 {
   public:
-					libxml2_doc(
-						std::istream&		data);
-					
-					libxml2_doc(
-						const std::string&	data);
+						libxml2_doc();
+						libxml2_doc(const std::string& s);
+						libxml2_doc(std::istream& is);
 
-					libxml2_doc(
-						node_ptr			data);
+	virtual				~libxml2_doc();
 
-	virtual			~libxml2_doc();
+	// I/O
+	void				read(const std::string& s);
+	void				read(std::istream& is);
+	void				read(std::istream& is, const boost::filesystem::path& base_dir);
 
-	node_ptr		root() const;
+	virtual void		write(writer& w) const;
+
+	// A valid xml libxml2_doc contains exactly one root element
+	root_node*			root() const;
+	
+	// and this root has one child xml::element
+	element*			child() const;
+	void				child(element* e);
+	
+	// helper functions
+	element_set			find(const std::string& path);
+	element*			find_first(const std::string& path);
+
+	// Compare two xml libxml2_docs
+	bool				operator==(const libxml2_doc& doc) const;
+
+	bool				operator!=(const libxml2_doc& doc) const		{ return not operator==(doc); }
+	
+	// In case the libxml2_doc needs to locate included DTD files
+	void				base_dir(const boost::filesystem::path& path);
+
+	// the encoding to use for output, or the detected encoding in the input
+	encoding_type		encoding() const;
+	void				encoding(encoding_type enc);
+
+	// to format the output, use the following:
+	
+	// number of spaces to indent elements:
+	int					indent() const;
+	void				indent(int indent);
+	
+	// whether to have each element on a separate line
+	bool				wrap() const;
+	void				wrap(bool wrap);
+	
+	// reduce the whitespace in #PCDATA sections
+	bool				trim() const;
+	void				trim(bool trim);
+
+	// option for parsing
+	void				set_validating(bool validate);
+
 
   private:
-	struct libxml2_doc_imp*	impl;
+	struct libxml2_doc_imp*	m_impl;
 };
 
+std::istream& operator>>(std::istream& lhs, libxml2_doc& rhs);
 std::ostream& operator<<(std::ostream& lhs, const libxml2_doc& rhs);
 
 }
