dcb6fc4076cd51b7ae33bff7aa82b56e9bb31c0f
maarten <maarten@31ceaa29-a5dd-4727-bcb8-6a3098495666>
2010-04-20 08:44:53 +0000

    missing
    git-svn-id: svn+ssh://mail.hekkelman.net/home/maarten/svnroot/libzeep/trunk@165 31ceaa29-a5dd-4727-bcb8-6a3098495666

---
+++ zeep/config.hpp
@@ -0,0 +1,19 @@
+//  Copyright Maarten L. Hekkelman, Radboud University 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef SOAP_CONFIG_H
+#define SOAP_CONFIG_H
+
+#ifndef SOAP_XML_HAS_EXPAT_SUPPORT
+#define SOAP_XML_HAS_EXPAT_SUPPORT 1
+#endif
+
+#ifndef nil
+#define nil NULL
+#endif
+
+// more defines here
+
+#endif
