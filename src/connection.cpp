7e02899cfa29fe154357f7aea5430927c2e636cc
Maarten L. Hekkelman <maarten@hekkelman.com>
2011-03-15 09:07:12 +0000

    keep-alive... sorry

--- src/connection.cpp
+++ src/connection.cpp
@@ -3,6 +3,8 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <iostream>
+
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
