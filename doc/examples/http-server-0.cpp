166588650c90c14335b784db5bcd1437dad311fe
Maarten L. Hekkelman <maarten@hekkelman.com>
2020-05-22 15:19:32 +0200

    new documentation

---
+++ doc/examples/http-server-0.cpp
@@ -0,0 +1,16 @@
+/* compile:
+clang++ -o http-server-0 http-server-0.cpp -I ../../include/  ../../lib/libzeep-http.a ../../lib/libzeep-xml.a ../../lib/libzeep-json.a -std=c++17 -lstdc++fs -I ~/projects/boost_1_73_0/ dummy_rsrc.o -pthread
+*/
+
+//[ most_simple_http_server
+#include <zeep/http/server.hpp>
+
+int main()
+{
+    zeep::http::server srv;
+    srv.bind("127.0.0.1", 8080);
+    srv.run(2);
+
+    return 0;
+}
+//]
