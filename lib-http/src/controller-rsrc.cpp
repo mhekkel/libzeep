e7248c830296fb1b879ea6bd7bc308e3007f625c
Maarten L. Hekkelman <maarten@hekkelman.com>
2020-09-05 13:38:19 +0200

    update for new mrc

--- lib-http/src/controller-rsrc.cpp
+++ lib-http/src/controller-rsrc.cpp
@@ -19,32 +19,6 @@ namespace zeep::http
 
 // -----------------------------------------------------------------------
 
-class rsrc_istream : public std::istream
-{
-  public:
-
-	~rsrc_istream()
-	{
-		delete m_buffer;
-	}
-
-	static rsrc_istream* create(const std::string& file)
-	{
-		mrsrc::rsrc rsrc(file);
-		return rsrc ? new rsrc_istream(new char_streambuf(rsrc.data(), rsrc.size())) : nullptr;
-	}
-
-  private:
-	rsrc_istream(std::streambuf* buffer)
-		: std::istream(buffer)
-		, m_buffer(buffer)
-	{
-	}
-
-	mrsrc::rsrc m_rsrc;
-	std::streambuf* m_buffer;
-};
-
 rsrc_loader::rsrc_loader(const std::string&)
 {
 	char exePath[PATH_MAX + 1];
@@ -75,10 +49,13 @@ fs::file_time_type rsrc_loader::file_time(const std::string& file, std::error_co
 // basic loader, returns error in ec if file was not found
 std::istream* rsrc_loader::load_file(const std::string& file, std::error_code& ec) noexcept
 {
-	std::istream* result = rsrc_istream::create(file);
+	mrsrc::rsrc resource(file);
 
-	if (result)
-		ec = {};
+	std::istream* result = nullptr;
+	ec = {};
+
+	if (resource)
+		result = new mrsrc::istream(resource);
 	else
 		ec = std::make_error_code(std::errc::no_such_file_or_directory);
 	
