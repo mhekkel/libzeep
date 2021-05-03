fb6b42c70dfd5bd041b026f44671fff938c871b1
Maarten L. Hekkelman <maarten@hekkelman.com>
2021-05-03 12:59:34 +0200

    fixed operator= for reply

--- lib-http/src/reply.cpp
+++ lib-http/src/reply.cpp
@@ -170,6 +170,10 @@ reply &reply::operator=(reply &&rhs)
 		m_data = rhs.m_data; rhs.m_data = nullptr;
 		m_headers = rhs.m_headers;
 		m_content = rhs.m_content;
+		m_chunked = rhs.m_chunked;
+
+		memcpy(m_size_buffer, rhs.m_size_buffer, sizeof(m_size_buffer));
+		m_status_line = rhs.m_status_line;
 	}
 
 	return *this;
