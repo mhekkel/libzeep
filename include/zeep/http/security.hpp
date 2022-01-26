f642fd99d090586edb4de87127074eecfa1dfabc
Maarten L. Hekkelman <maarten@hekkelman.com>
2022-01-26 11:16:02 +0100

    Update copyright, changelog

--- include/zeep/http/security.hpp
+++ include/zeep/http/security.hpp
@@ -1,4 +1,4 @@
-//        Copyright Maarten L. Hekkelman, 2014-2020
+//        Copyright Maarten L. Hekkelman, 2014-2022
 //   Distributed under the Boost Software License, Version 1.0.
 //      (See accompanying file LICENSE_1_0.txt or copy at
 //            http://www.boost.org/LICENSE_1_0.txt)
