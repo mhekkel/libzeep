11ce316032e0f9d767e2df86905b920fbd7d80b6
Maarten L. Hekkelman <maarten@hekkelman.com>
2023-08-22 13:21:50 +0200

    copy rights

--- include/zeep/http/daemon.hpp
+++ include/zeep/http/daemon.hpp
@@ -1,4 +1,4 @@
-//        Copyright Maarten L. Hekkelman, 2014-2022
+//        Copyright Maarten L. Hekkelman, 2014-2023
 //  Distributed under the Boost Software License, Version 1.0.
 //     (See accompanying file LICENSE_1_0.txt or copy at
 //           http://www.boost.org/LICENSE_1_0.txt)
