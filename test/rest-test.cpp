fea0abc9e95a7596a54489e450615b402bf12c74
Maarten L. Hekkelman <maarten@hekkelman.com>
2024-03-12 16:15:16 +0100

    cleaning up

--- test/rest-test.cpp
+++ test/rest-test.cpp
@@ -1,8 +1,8 @@
-#include <zeep/http/rest-controller.hpp>
-#include <zeep/http/daemon.hpp>
 #include <zeep/exception.hpp>
-#include <zeep/nvp.hpp>
+#include <zeep/http/daemon.hpp>
+#include <zeep/http/rest-controller.hpp>
 #include <zeep/json-serializer.hpp>
+#include <zeep/nvp.hpp>
 
 #include "../src/signals.hpp"
 
@@ -17,45 +17,50 @@ using namespace std;
 namespace z = zeep;
 namespace zh = zeep::http;
 
-
 struct Opname
 {
-	string				id;
-	map<string,float>	standen;
+	string id;
+	map<string, float> standen;
 
-	template<typename Archive>
-	void serialize(Archive& ar, unsigned long /*version*/)
+	template <typename Archive>
+	void serialize(Archive &ar, unsigned long /*version*/)
 	{
-		ar & zeep::make_nvp("id", id)
-		   & zeep::make_nvp("standen", standen);
+		ar &zeep::make_nvp("id", id) & zeep::make_nvp("standen", standen);
 	}
 };
 
 enum class aggregatie_type
 {
-	dag, week, maand, jaar
+	dag,
+	week,
+	maand,
+	jaar
 };
 
-void to_json(nlohmann::json& e, aggregatie_type aggregatie)
+void to_json(nlohmann::json &e, aggregatie_type aggregatie)
 {
 	switch (aggregatie)
 	{
-		case aggregatie_type::dag:		e = "dag"; break;
-		case aggregatie_type::week:		e = "week"; break;
-		case aggregatie_type::maand:	e = "maand"; break;
-		case aggregatie_type::jaar:		e = "jaar"; break;
+		case aggregatie_type::dag: e = "dag"; break;
+		case aggregatie_type::week: e = "week"; break;
+		case aggregatie_type::maand: e = "maand"; break;
+		case aggregatie_type::jaar: e = "jaar"; break;
 	}
 }
 
-void from_json(const nlohmann::json& e, aggregatie_type& aggregatie)
+void from_json(const nlohmann::json &e, aggregatie_type &aggregatie)
 {
-	if (e == "dag")				aggregatie = aggregatie_type::dag;
-	else if (e == "week")		aggregatie = aggregatie_type::week;
-	else if (e == "maand")		aggregatie = aggregatie_type::maand;
-	else if (e == "jaar")		aggregatie = aggregatie_type::jaar;
-	else throw runtime_error("Ongeldige aggregatie");
+	if (e == "dag")
+		aggregatie = aggregatie_type::dag;
+	else if (e == "week")
+		aggregatie = aggregatie_type::week;
+	else if (e == "maand")
+		aggregatie = aggregatie_type::maand;
+	else if (e == "jaar")
+		aggregatie = aggregatie_type::jaar;
+	else
+		throw runtime_error("Ongeldige aggregatie");
 }
-
 
 enum class grafiek_type
 {
@@ -71,50 +76,59 @@ enum class grafiek_type
 	electriciteit_levering_laag
 };
 
-void to_json(nlohmann::json& e, grafiek_type type)
+void to_json(nlohmann::json &e, grafiek_type type)
 {
 	switch (type)
 	{
-		case grafiek_type::warmte:						e = "warmte"; break;
-		case grafiek_type::electriciteit:				e = "electriciteit"; break;
-		case grafiek_type::electriciteit_hoog:			e = "electriciteit-hoog"; break;
-		case grafiek_type::electriciteit_laag:			e = "electriciteit-laag"; break;
-		case grafiek_type::electriciteit_verbruik:		e = "electriciteit-verbruik"; break;
-		case grafiek_type::electriciteit_levering:		e = "electriciteit-levering"; break;
-		case grafiek_type::electriciteit_verbruik_hoog:	e = "electriciteit-verbruik-hoog"; break;
-		case grafiek_type::electriciteit_verbruik_laag:	e = "electriciteit-verbruik-laag"; break;
-		case grafiek_type::electriciteit_levering_hoog:	e = "electriciteit-levering-hoog"; break;
-		case grafiek_type::electriciteit_levering_laag:	e = "electriciteit-levering-laag"; break;
+		case grafiek_type::warmte: e = "warmte"; break;
+		case grafiek_type::electriciteit: e = "electriciteit"; break;
+		case grafiek_type::electriciteit_hoog: e = "electriciteit-hoog"; break;
+		case grafiek_type::electriciteit_laag: e = "electriciteit-laag"; break;
+		case grafiek_type::electriciteit_verbruik: e = "electriciteit-verbruik"; break;
+		case grafiek_type::electriciteit_levering: e = "electriciteit-levering"; break;
+		case grafiek_type::electriciteit_verbruik_hoog: e = "electriciteit-verbruik-hoog"; break;
+		case grafiek_type::electriciteit_verbruik_laag: e = "electriciteit-verbruik-laag"; break;
+		case grafiek_type::electriciteit_levering_hoog: e = "electriciteit-levering-hoog"; break;
+		case grafiek_type::electriciteit_levering_laag: e = "electriciteit-levering-laag"; break;
 	}
 }
 
-void from_json(const nlohmann::json& e, grafiek_type& type)
+void from_json(const nlohmann::json &e, grafiek_type &type)
 {
-		 if (e == "warmte")						 type = grafiek_type::warmte;						
-	else if (e == "electriciteit")				 type = grafiek_type::electriciteit;				
-	else if (e == "electriciteit-hoog")			 type = grafiek_type::electriciteit_hoog;			
-	else if (e == "electriciteit-laag")			 type = grafiek_type::electriciteit_laag;			
-	else if (e == "electriciteit-verbruik")		 type = grafiek_type::electriciteit_verbruik;		
-	else if (e == "electriciteit-levering")		 type = grafiek_type::electriciteit_levering;		
-	else if (e == "electriciteit-verbruik-hoog") type = grafiek_type::electriciteit_verbruik_hoog;	
-	else if (e == "electriciteit-verbruik-laag") type = grafiek_type::electriciteit_verbruik_laag;	
-	else if (e == "electriciteit-levering-hoog") type = grafiek_type::electriciteit_levering_hoog;	
-	else if (e == "electriciteit-levering-laag") type = grafiek_type::electriciteit_levering_laag;	
-	else throw runtime_error("Ongeldige grafiek type");
+	if (e == "warmte")
+		type = grafiek_type::warmte;
+	else if (e == "electriciteit")
+		type = grafiek_type::electriciteit;
+	else if (e == "electriciteit-hoog")
+		type = grafiek_type::electriciteit_hoog;
+	else if (e == "electriciteit-laag")
+		type = grafiek_type::electriciteit_laag;
+	else if (e == "electriciteit-verbruik")
+		type = grafiek_type::electriciteit_verbruik;
+	else if (e == "electriciteit-levering")
+		type = grafiek_type::electriciteit_levering;
+	else if (e == "electriciteit-verbruik-hoog")
+		type = grafiek_type::electriciteit_verbruik_hoog;
+	else if (e == "electriciteit-verbruik-laag")
+		type = grafiek_type::electriciteit_verbruik_laag;
+	else if (e == "electriciteit-levering-hoog")
+		type = grafiek_type::electriciteit_levering_hoog;
+	else if (e == "electriciteit-levering-laag")
+		type = grafiek_type::electriciteit_levering_laag;
+	else
+		throw runtime_error("Ongeldige grafiek type");
 }
 
 struct GrafiekData
 {
-	string 				type;
-	map<string,float>	punten;
-	map<string,float>   vsGem;
+	string type;
+	map<string, float> punten;
+	map<string, float> vsGem;
 
-	template<typename Archive>
-	void serialize(Archive& ar, unsigned long)
+	template <typename Archive>
+	void serialize(Archive &ar, unsigned long)
 	{
-		ar & zeep::make_nvp("type", type)
-		   & zeep::make_nvp("punten", punten)
-		   & zeep::make_nvp("vsgem", vsGem);
+		ar &zeep::make_nvp("type", type) & zeep::make_nvp("punten", punten) & zeep::make_nvp("vsgem", vsGem);
 	}
 };
 
@@ -185,19 +199,16 @@ class e_rest_controller : public zeep::http::rest_controller
 
 	GrafiekData get_grafiek(grafiek_type /*type*/, grafiek_type /*type*/)
 	{
-		return{};
+		return {};
 	}
 
 	zeep::http::reply get_all_data()
 	{
-		return { zeep::http::ok, { 1, 0 }, {
-			{ "Content-Length", "13" },
-			{ "Content-Type", "text/plain" }
-		}, "Hello, world!" };
+		return { zeep::http::ok, { 1, 0 }, { { "Content-Length", "13" }, { "Content-Type", "text/plain" } }, "Hello, world!" };
 	}
 };
 
-TEST_CASE("rest_1")	
+TEST_CASE("rest_1")
 {
 	// simply see if the above compiles
 
@@ -211,20 +222,21 @@ TEST_CASE("rest_1")
 	zeep::http::request req{ "GET", "/ajax/all_data" };
 
 	CHECK(rc.dispatch_request(s, req, rep));
-	
+
 	CHECK(rep.get_status() == zeep::http::ok);
 	CHECK(rep.get_content_type() == "text/plain");
 }
 
-TEST_CASE("rest_2")	
+TEST_CASE("rest_2")
 {
 	// start up a http server and stop it again
 
-	zh::daemon d([]() {
+	zh::daemon d([]()
+		{
 		auto s = new zh::server;
 		s->add_controller(new e_rest_controller());
-		return s;
-	}, "zeep-http-test");
+		return s; },
+		"zeep-http-test");
 
 	std::random_device rng;
 	uint16_t port = 1024 + (rng() % 10240);
@@ -250,7 +262,7 @@ TEST_CASE("rest_2")
 		CHECK(reply.get_status() == zh::not_found);
 		CHECK(reply.get_content_type() == "application/json");
 	}
-	catch (const std::exception& e)
+	catch (const std::exception &e)
 	{
 		std::clog << e.what() << '\n';
 	}
