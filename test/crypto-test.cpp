a362b631e2d0f9e0ce457444df6674e7b2e81c8a
Maarten L. Hekkelman <maarten@hekkelman.com>
2026-02-26 11:42:40 +0100

    Revive daemon-test example, version bump

--- test/crypto-test.cpp
+++ test/crypto-test.cpp
@@ -103,7 +103,10 @@ TEST_CASE("crypto_sha256_1")
 
 TEST_CASE("crypto_hmac_1")
 {
-	auto h = zeep::encode_hex(zeep::hmac_sha256("The quick brown fox jumps over the lazy dog", "key"));
+	auto h = zeep::encode_base64(zeep::hmac_sha1("The quick brown fox jumps over the lazy dog", "key"));
+	CHECK(h == "de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9");
+
+	h = zeep::encode_hex(zeep::hmac_sha256("The quick brown fox jumps over the lazy dog", "key"));
 	CHECK(h == "f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8");
 
 	h = zeep::encode_hex(zeep::hmac_md5("The quick brown fox jumps over the lazy dog", "key"));
