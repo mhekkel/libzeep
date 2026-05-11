b6787bb7699d4cf8b88e5f608f1879ed3b3c35fa
Maarten L. Hekkelman <maarten@hekkelman.com>
2026-05-11 13:36:19 +0200

    forgejo actions

---
+++ .github/README.md
@@ -0,0 +1,4 @@
+Moved
+=====
+
+This project has moved to https://forge.hekkelman.net/maarten/zeep.git
--- cmake/test-chrono-zoned_time.cpp
+++ cmake/test-chrono-zoned_time.cpp
@@ -6,5 +6,6 @@ int main()
 	auto now = std::chrono::system_clock::now();
 	auto t = std::chrono::zoned_time{ std::chrono::current_zone(), std::chrono::floor<std::chrono::seconds>(now) };
 	std::cout << std::format("{:%d/%b/%Y:%H:%M:%S %Ez}", t);
+
 	return 0;
 }
